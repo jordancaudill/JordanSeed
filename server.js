0d028e7e488648bced824aadc83d73ba3863a468
Jordan Caudill <jacaudill74@gmail.com>
2016-11-21 15:17:00 -0600

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+/*jshint node:true*/
+'use strict';
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.listen(port, function () {
+    console.log('Server is running on localhost:' + port);
+});
