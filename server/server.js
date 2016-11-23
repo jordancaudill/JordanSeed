f37993a97ded7ad8ebc26ce6c3e75cd6245e32fd
Jordan Caudill <jacaudill74@gmail.com>
2016-11-23 14:07:13 -0600

    Removed gulp, added MongoDB

--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 /*jshint node:true*/
 'use strict';
 var express = require('express');
+var mongoose = require('mongoose');
+var mongodb = require('mongodb');
+var async = require('async');
+
 var app = express();
 var port = 3000;
 
+// Start the express server on the given port
 app.listen(port, function () {
     console.log('Server is running on localhost:' + port);
 });
 
+// Serve the static front end files
 app.use(express.static('../client'));
+
+// Connect to MongoDB, then Mongoose
+var dbString = 'mongodb://localhost/testDB';
+async.series([
+    function(callback) {
+        mongodb.connect(dbString, function (err, res) {
+            callback(err, res);
+        });
+    },
+    function(callback) {
+         mongoose.connect(dbString, function(err, res) {
+             callback(err, res);
+        });
+    }
+],
+// This is the callback
+function(err, results) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully connected to MongoDB and Mongoose.');
+    }
+});
