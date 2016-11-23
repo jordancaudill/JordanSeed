422ad74c6041aab9ae7c4d2eed92bc25340bfcf2
Jordan Caudill <jacaudill74@gmail.com>
2016-11-23 14:39:12 -0600

    aewrsdt

---
+++ api/users.js
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+
+// TODO: unsure if sign up should be 
+router.post('/signup', signup);
+
+function signup(req, res, next) {
+    
+}
+
+// Make these endpoints available
+module.exports = router;
