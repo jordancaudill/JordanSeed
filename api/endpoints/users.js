132fb55c7f5c2e139eaa91631d536fb6e7c54161
Jordan Caudill <jacaudill74@gmail.com>
2016-12-13 11:34:30 -0600

    first unit tests

--- api/endpoints/users.js
+++ api/endpoints/users.js
@@ -1,15 +1,30 @@
 var router = require('express').Router();
-var userSchema = require('../schemas/userSchema');
+var User = require('../schemas/userSchema');
 
 router.post('/users', createUser);
+router.get('/users', getUsers);
 
 function createUser(req, res, next) {
-    console.log('creating user:', req.body);
-    userSchema.create(req.body, function (err, user) {
-        if (err) {
-            return res.status(400).send(err);
+    User.findOne({ email: req.body.email }, function (err, existingUser) {
+        if (existingUser) {
+            return res.status(400).send('A user with that email already exists.');
+        } else {
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    return res.status(400).send(err);
+                }
+                return res.status(201).send(user);
+            });
         }
-        return res.status(201).send(user);
+    });
+}
+
+function getUsers(req, res, next) {
+    User.find({}, function (err, users) {
+         if (err) {
+             return res.status(400).send(err);
+         }
+         return res.status(200).send(users);
     });
 }
 
--- api/test/user.test.js
+++ api/test/user.test.js
@@ -5,19 +5,84 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('users api', function() {
-    // it('should list ALL users on /users GET', function(done) {
-    // });
+    describe('/users POST endpoint', function() {
+        it('should add a user with valid data', function(done) {
+            var validUser = {'firstName': 'validName', 'lastName': 'validLastName', 'email': new Date().getTime(), 'password': 'validPassword'};
+            chai.request(server)
+                .post('/users')
+                .send(validUser)
+                .end(function(err, res){
+                    res.should.have.status(201);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('firstName');
+                    res.body.should.have.property('lastName');
+                    res.body.should.have.property('email');
+                    res.body.should.have.property('password');
+                    res.body.should.have.property('_id');
+                    done();
+                });
+        });
 
-    it('should add a SINGLE user on /users POST', function(done) {
-        var user = {'firstName': 'validName', 'lastName': 'validLastName', 'email': 'validEmail@jsman.com', 'password': 'validPassword'};
-        chai.request(server)
-            .post('/users')
-            .send(user)
-            .end(function(err, res){
-                res.should.have.status(201);
-                done();
-            });
+        it('should NOT add a user with invalid data', function(done) {
+            var inValidUser = {};
+            chai.request(server)
+                .post('/users')
+                .send(inValidUser)
+                .end(function(err, res){
+                    res.should.have.status(400);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
+        });
+
+        it('should NOT add a user with an existing email address', function(done) {
+            var uniqueEmail = new Date().getTime();
+            var user = {'firstName': 'validName', 'lastName': 'validLastName', 'email': uniqueEmail, 'password': 'validPassword'};
+            chai.request(server)
+                .post('/users')
+                .send(user)
+                .end(function(err, res){
+                     chai.request(server)
+                    .post('/users')
+                    .send(user)
+                    .end(function(err, res){
+                        res.should.have.status(400);
+                        res.error.text.should.be.a('string');
+                        res.error.text.should.equal('A user with that email already exists.');
+                        done();
+                    });
+                });
+        });
     });
+
+    describe('/users GET endpoint', function() {
+        it('should list ALL users', function(done) {
+            chai.request(server)
+                .get('/users')
+                .send()
+                .end(function(err, res){
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+
+    describe('/users/:id GET endpoint', function() {
+        it('should return a user with a valid ID', function(done) {
+
+        });
+
+        it('should NOT return a user with an invalid ID', function(done) {
+
+        });
+    });
+
+
     it('should list a SINGLE user on /user/<id> GET');
     it('should update a SINGLE user on /user/<id> PUT');
     it('should delete a SINGLE user on /user/<id> DELETE');
